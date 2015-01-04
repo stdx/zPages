04f9ce061dfe3c11a0f8c6ef9676c67309446b8e
Stefan Dahms <std@stdx.de>
2015-01-04 22:28:31 +0100

    Removed faulty insertions.

--- module/Company/src/Company/Entity/company.sql
+++ module/Company/src/Company/Entity/company.sql
@@ -21,24 +21,3 @@ CREATE TABLE `product` (
 
 ALTER TABLE  `product` ADD INDEX (  `company_id` ) ;
 ALTER TABLE  `product` ADD INDEX (  `product_template_id` ) ;
-
- INSERT INTO company (name, description)
-     VALUES  ('The  Military  Wives',  'In  My  Dreams');
- INSERT INTO company (name, description)
-     VALUES  ('Adele',  '21');
- INSERT INTO company (name, description)
-     VALUES  ('Bruce  Springsteen',  'Wrecking Ball (Deluxe)');
- INSERT INTO company (name, description)
-     VALUES  ('Lana  Del  Rey',  'Born  To  Die');
- INSERT INTO company (name, description)
-     VALUES  ('Gotye',  'Making  Mirrors');
-     
-     
-INSERT INTO product_template (name, description, company_id)
-    VALUES  ('Product 1',  'Product Description', 1);
-    
-INSERT INTO product_template (name, description, company_id)
-    VALUES  ('Product 1',  'Product Description', 1);
-    
-INSERT INTO product_template (name, description, company_id)
-    VALUES  ('Product 1',  'Product Description', 1);
